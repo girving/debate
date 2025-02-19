23bb8f014e8f9c25309a9197459c5727361c05ba
Eric Wieser <wieser.eric@gmail.com>
2025-02-19 02:36:57 +0000

    One oracle is all you need

--- Comp/Defs.lean
+++ Comp/Defs.lean
@@ -1,6 +1,7 @@
 import Comp.Oracle
 import Prob.Defs
 import Prob.Fin
+import DComp.Defs
 
 /-!
 ## Oracle-relative probabilistic computations
@@ -22,17 +23,18 @@ variable {I : Type} {ι : I → Type} {ω : {o : I} → ι o → Type}
 variable {s t : Set I}
 variable {α β γ : Type}
 
+
 /-- A stochastic computation that can make oracle queries.
     Importantly, the computation does not know the oracle, so we can model query complexity.
     The `Comp` constructors are not very user friendly due to kernel restrictions on inductive,
     but we replace them with clean ones below. -/
-inductive Comp (ι : I → Type) (ω : {o : I} → ι o → Type) (s : Set I) (α : Type) : Type where
-  /-- Return a result with no computation -/
-  | pure' : α → Comp ι ω s α
-  /-- Sample a value with some probability distribution, then continue -/
-  | sample' : {n : ℕ} → Prob (Fin n) → (Fin n → Comp ι ω s α) → Comp ι ω s α
-  /-- Query an oracle `o ∈ s`, and branch on the result -/
-  | query' : (o : I) → o ∈ s → (y : ι o) → ((ω y) → Comp ι ω s α) → Comp ι ω s α
+def Comp (ι : I → Type) (ω : {o : I} → ι o → Type) (s : Set I) (α : Type) : Type :=
+  DComp
+    (I := Option I)
+    (ι := fun | some i => ι i | none => Σ n, Prob (Fin n))
+    (ω := @fun | some _ => ω | none => fun s => Fin s.1)
+    (some '' s ∪ {none})
+    α
 
 /-- `Comp` where all oracles accept `ι` and return `Bool` -/
 abbrev BComp (ι : Type) {I : Type} (s : Set I) (α : Type) := Comp (fun _ => ι) (fun _ ↦ Bool) s α
@@ -40,15 +42,20 @@ abbrev BComp (ι : Type) {I : Type} (s : Set I) (α : Type) := Comp (fun _ => ι
 namespace Comp
 
 /-- Bind two `Comp`s together -/
-def bind' (f : Comp ι ω s α) (g : α → Comp ι ω s β) : Comp ι ω s β := match f with
-  | .pure' x => g x
-  | .sample' p f => .sample' p (fun y ↦ (f y).bind' g)
-  | .query' o m y f => .query' o m y fun x ↦ (f x).bind' g
+nonrec def bind' (f : Comp ι ω s α) (g : α → Comp ι ω s β) : Comp ι ω s β := f.bind' g
+
 
 /-- `Comp` is a monad -/
 instance : Monad (Comp ι ω s) where
-  pure := Comp.pure'
+  pure := DComp.pure'
   bind := Comp.bind'
+
+nonrec def query'' (o : I) : o ∈ s → (y : ι o) → ((ω y) → Comp ι ω s α) → Comp ι ω s α :=
+  fun h y f => .query' (some o) (by simp [h]) y f
+
+@[match_pattern]
+def sample' {n} (p : Prob (Fin n)) (f : Fin n → Comp ι ω s β) : Comp ι ω s β :=
+  DComp.query' none (by simp) ⟨n, p⟩ f
 
 /-- `sample'` with a general `α` instead of `Fin n` -/
 def sample (p : Prob α) (f : α → Comp ι ω s β) : Comp ι ω s β :=
@@ -60,13 +67,13 @@ instance : Coe (Prob α) (Comp ι ω s α) where
 
 /-- The simplest case of `Comp.query'` -/
 def query (i : I) (y : ι i) : Comp ι ω {i} (ω y) :=
-  Comp.query' i (mem_singleton _) y pure
+  Comp.query'' i (mem_singleton _) y pure
 
 /-- The value and query counts of a `Comp ι s`, once we supply oracles -/
 def run (f : Comp ι ω s α) (o : (i : I) → Oracle (ι i) ω) : Prob (α × (I → ℕ)) := match f with
   | .pure' x => pure (x, fun _ => 0)
   | .sample' f g => f >>= fun x ↦ (g x).run o
-  | .query' i _ y f => do
+  | .query'' i _ y f => do
     let x ← o i y
     let (z,c) ← (f x).run o
     return (z, c + fun j => if j = i then 1 else 0)
